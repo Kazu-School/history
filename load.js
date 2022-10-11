4791d21210d09304eaa115ce57bcd4c81af59fbe
kazu-school <111211945+Kazu-School@users.noreply.github.com>
2022-10-11 13:01:44 +0900

    Update load.js

--- load.js
+++ load.js
@@ -1,8 +1,17 @@
 import data from './historydata.json' assert {type: 'json'}
 const loads =() =>{
-  let datalist=[];
-  for (let item in data){
-    datalist.push('<li class="list">'+item+'</li>');
+  for (let i=0; i < data.length; i++){
+    var mytable = document.getElementById("table");
+    var mytr = mytable.insertRow(-1);
+    var myth = document.createElement("th");
+    myth.innerHTML = data[i][0];
+    mytr.appendChild(myth);
+    var ans = mytr.insertCell(1);
+    ans.innerHTML = data[i][1];
+    for (let n=2; i < data[i].length; n++){
+      var def = mytr.insertCell(n);
+      def.innerHTML=data[i][n]
+    };
   };
   txt.innerHTML = datalist.join('');
   console.table(data);
